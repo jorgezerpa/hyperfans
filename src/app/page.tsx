7ecd0e4fd75df4a407070c1aa79029dbf8155aca
Jorge Zerpa <jorgezerpacoder@gmail.com>
2025-01-10 16:03:19 -0400

    Basic setup of Stream SDK

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,71 @@
-import Image from "next/image";
+'use client'
+import {
+  StreamVideoClient,
+  StreamVideo,
+  User,
+  StreamCall,
+} from "@stream-io/video-react-sdk";
+
+// for livestreamview component 
+import { ParticipantView, useCallStateHooks } from '@stream-io/video-react-sdk'; 
+
+const apiKey = "mmhfdzb5evj2";
+// this token should be generated individually for the user on sign-up or login (is a common JSON web token)
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL1F1aS1Hb25fSmlubiIsInVzZXJfaWQiOiJRdWktR29uX0ppbm4iLCJ2YWxpZGl0eV9pbl9zZWNvbmRzIjo2MDQ4MDAsImlhdCI6MTczNjUzNjgwOSwiZXhwIjoxNzM3MTQxNjA5fQ.KYW_tZ1H6AYdn6Pc-gdU8d5XtQfjqkE1Oksh-Yl8gMc";
+const userId = "Qui-Gon_Jinn";
+const callId = "XEy9p9volvGq";
+
+const user: User = { id: userId, name: "Tutorial" };
+const client = new StreamVideoClient({ apiKey, user, token });
+const call = client.call("livestream", callId);
+call.join({ create: true });
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <StreamVideo client={client}>
+      <StreamCall call={call}>
+        <LivestreamView />
+      </StreamCall>
+    </StreamVideo>
   );
 }
+
+const LivestreamView = () => {
+  const {
+    useCameraState,
+    useMicrophoneState,
+    useParticipantCount,
+    useIsCallLive,
+    useParticipants,
+  } = useCallStateHooks();
+
+  const { camera: cam, isEnabled: isCamEnabled } = useCameraState();
+  const { microphone: mic, isEnabled: isMicEnabled } = useMicrophoneState();
+  
+  const participantCount = useParticipantCount();
+  const isLive = useIsCallLive();
+
+  const [firstParticipant] = useParticipants();
+  
+  return (
+    <div style={{ display: "flex", flexDirection: 'column', gap: '4px' }}>
+      <div>{isLive ? `Live: ${participantCount}`: `In Backstage`}</div>
+      {firstParticipant ? (
+        <ParticipantView participant={firstParticipant} />
+      ) : (
+        <div>The host hasn't joined yet</div>
+      )}
+      <div style={{ display: 'flex', gap: '4px'}}>
+        <button onClick={() => (isLive ? call.stopLive() : call.goLive())}>
+          {isLive ? "Stop Live" : "Go Live"}
+        </button>
+        <button onClick={() => cam.toggle()}>
+          {isCamEnabled ? "Disable camera" : "Enable camera"}
+        </button>
+        <button onClick={() => mic.toggle()}>
+          {isMicEnabled ? "Mute Mic" : "Unmute Mic"}
+        </button>
+      </div>
+    </div>
+  );
+};
