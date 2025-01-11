34960f49b11b84d3525a4a1b344a8849e69dfaca
Jorge Zerpa <jorgezerpacoder@gmail.com>
2025-01-11 08:01:56 -0400

    Modify login logic to search for users on our Prisma DB

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,48 @@
 import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "../../../../../lib/prisma";
+import bcrypt from "bcrypt"
 
 const handler = NextAuth({
-    // Configure one or more authentication providers
     providers: [
         CredentialsProvider({
-          // The name to display on the sign in form (e.g. "Sign in with...")
           name: "Credentials",
-          // `credentials` is used to generate a form on the sign in page.
-          // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-          // e.g. domain, username, password, 2FA token, etc.
-          // You can pass any HTML attribute to the <input> tag through the object.
           credentials: {
-            username: { label: "Username", type: "text", placeholder: "jsmith" },
+            username: { label: "Email", type: "text", placeholder: "jsmith@gmail.com" },
             password: { label: "Password", type: "password" }
           },
           async authorize(credentials, req) {
-            // Add logic here to look up the user from the credentials supplied
-            const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+            if (!credentials) return null;
+            // get user from DB using its email
+            const user = await prisma.user.findUnique({
+              where: {
+                  email: credentials.username
+              }
+            });
+
+            if (!user) return null;
+            if(!user.password) return null;
+
+            // comparing provided password with stored in DB
+            const match = await bcrypt.compare(credentials.password, user.password);
+            if (!match) return null;
+
+            return { 
+              id: user.id.toString(), 
+              name: user.name,
+              email: user.email,
+            };
       
-            if (user) {
-              // Any object returned will be saved in `user` property of the JWT
-              return user
-            } else {
-              // If you return null then an error will be displayed advising the user to check their details.
-              return null
+            // const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+            // if (user) {
+            //   // Any object returned will be saved in `user` property of the JWT
+            //   return user
+            // } else {
+            //   // If you return null then an error will be displayed advising the user to check their details.
+            //   return null
       
-              // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-            }
+            //   // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+            // }
           }
         })
     ]
