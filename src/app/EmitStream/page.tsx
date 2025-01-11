2455ba9495b09afe1f2d4820ebc855a2704ccbe1
Jorge Zerpa <jorgezerpacoder@gmail.com>
2025-01-11 11:12:35 -0400

    Create stream client with Anonimous user

--- src/app/EmitStream/page.tsx
+++ src/app/EmitStream/page.tsx
@@ -13,14 +13,16 @@ const apiKey = "mmhfdzb5evj2";
 // this token should be generated individually for the user on sign-up or login (is a common JSON web token)
 // this token is to control who can access to the call 
 // should be getted from the Stream SDK 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL1F1aS1Hb25fSmlubiIsInVzZXJfaWQiOiJRdWktR29uX0ppbm4iLCJ2YWxpZGl0eV9pbl9zZWNvbmRzIjo2MDQ4MDAsImlhdCI6MTczNjUzNjgwOSwiZXhwIjoxNzM3MTQxNjA5fQ.KYW_tZ1H6AYdn6Pc-gdU8d5XtQfjqkE1Oksh-Yl8gMc";
-const userId = "Qui-Gon_Jinn";
-const callId = "XEy9p9volvGq";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0FheWxhX1NlY3VyYSIsInVzZXJfaWQiOiJBYXlsYV9TZWN1cmEiLCJ2YWxpZGl0eV9pbl9zZWNvbmRzIjo2MDQ4MDAsImlhdCI6MTczNjYwNDYyOSwiZXhwIjoxNzM3MjA5NDI5fQ.oFefl01pehR1edVFF2cYx7aOEL6pFX12kJrdY0ez_nk";
+const userId = "Aayla_Secura";
+const callId = "wBgOIUVE6tna";
 
 const user: User = { id: userId, name: "Tutorial" };
 const client = new StreamVideoClient({ apiKey, user, token });
 const call = client.call("livestream", callId);
 call.join({ create: true });
+
+console.log(call.id)
 
 export default function Home() {
   return (
--- src/app/ViewLiveStream/page.tsx
+++ src/app/ViewLiveStream/page.tsx
@@ -1,76 +1,28 @@
-'use client'
+"use client"
 import {
-  StreamVideoClient,
+  LivestreamPlayer,
   StreamVideo,
+  StreamVideoClient,
   User,
-  StreamCall,
 } from "@stream-io/video-react-sdk";
 
-// for livestreamview component 
-import { ParticipantView, useCallStateHooks } from '@stream-io/video-react-sdk'; 
+const apiKey = "tqag879npxda";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJAc3RyZWFtLWlvL2Rhc2hib2FyZCIsImlhdCI6MTczNjYwNzc0MCwiZXhwIjoxNzM2Njk0MTQwLCJ1c2VyX2lkIjoiIWFub24iLCJyb2xlIjoidmlld2VyIiwiY2FsbF9jaWRzIjpbImxpdmVzdHJlYW06bGl2ZXN0cmVhbV8xMzQ3ZWM1Yy0xNGU4LTRkNDctODdkOC1iYmRlOWE3YjhkMGMiXX0.IGDs8daHV5rBASuSOpm8GgaZ-tU2aUr8ARE5m5ErUdQ";
+const callId = "livestream_1347ec5c-14e8-4d47-87d8-bbde9a7b8d0c";
 
-const apiKey = "mmhfdzb5evj2";
-// this token should be generated individually for the user on sign-up or login (is a common JSON web token)
-// this token is to control who can access to the call 
-// should be getted from the Stream SDK 
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL1F1aS1Hb25fSmlubiIsInVzZXJfaWQiOiJRdWktR29uX0ppbm4iLCJ2YWxpZGl0eV9pbl9zZWNvbmRzIjo2MDQ4MDAsImlhdCI6MTczNjUzNjgwOSwiZXhwIjoxNzM3MTQxNjA5fQ.KYW_tZ1H6AYdn6Pc-gdU8d5XtQfjqkE1Oksh-Yl8gMc";
-const userId = "Qui-Gon_Jinn";
-const callId = "XEy9p9volvGq";
-
-const user: User = { id: userId, name: "Tutorial" };
+const user: User = { type: "anonymous" };
 const client = new StreamVideoClient({ apiKey, user, token });
-const call = client.call("livestream", callId);
-call.join({ create: true });
 
-export default function Home() {
+export default function App() {
   return (
     <StreamVideo client={client}>
-      <StreamCall call={call}>
-        <LivestreamView />
-      </StreamCall>
+      <LivestreamPlayer callType="livestream" callId={callId} />
     </StreamVideo>
   );
 }
 
-const LivestreamView = () => {
-  const {
-    useCameraState,
-    useMicrophoneState,
-    useParticipantCount,
-    useIsCallLive,
-    useParticipants,
-  } = useCallStateHooks();
 
-  const { camera: cam, isEnabled: isCamEnabled } = useCameraState();
-  const { microphone: mic, isEnabled: isMicEnabled } = useMicrophoneState();
-  
-  const participantCount = useParticipantCount();
-  const isLive = useIsCallLive();
 
-  const [firstParticipant] = useParticipants();
-  
-  return (
-    <div style={{ display: "flex", flexDirection: 'column', gap: '4px' }}>
-      <div>{isLive ? `Live: ${participantCount}`: `In Backstage`}</div>
-      {firstParticipant ? (
-        <ParticipantView participant={firstParticipant} />
-      ) : (
-        <div>The host hasn't joined yet</div>
-      )}
-      <div style={{ display: 'flex', gap: '4px'}}>
-        <button onClick={() => (isLive ? call.stopLive() : call.goLive())}>
-          {isLive ? "Stop Live" : "Go Live"}
-        </button>
-        <button onClick={() => cam.toggle()}>
-          {isCamEnabled ? "Disable camera" : "Enable camera"}
-        </button>
-        <button onClick={() => mic.toggle()}>
-          {isMicEnabled ? "Mute Mic" : "Unmute Mic"}
-        </button>
-      </div>
-    </div>
-  );
-};
 
 // 'use client'
 // import {
@@ -78,37 +30,70 @@ const LivestreamView = () => {
 //   StreamVideo,
 //   User,
 //   StreamCall,
-//   LivestreamLayout
 // } from "@stream-io/video-react-sdk";
 
 // // for livestreamview component 
+// import { ParticipantView, useCallStateHooks } from '@stream-io/video-react-sdk'; 
 
 // const apiKey = "mmhfdzb5evj2";
 // // this token should be generated individually for the user on sign-up or login (is a common JSON web token)
+// // this token is to control who can access to the call 
+// // should be getted from the Stream SDK 
 // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL1F1aS1Hb25fSmlubiIsInVzZXJfaWQiOiJRdWktR29uX0ppbm4iLCJ2YWxpZGl0eV9pbl9zZWNvbmRzIjo2MDQ4MDAsImlhdCI6MTczNjUzNjgwOSwiZXhwIjoxNzM3MTQxNjA5fQ.KYW_tZ1H6AYdn6Pc-gdU8d5XtQfjqkE1Oksh-Yl8gMc";
 // const userId = "Qui-Gon_Jinn";
 // const callId = "XEy9p9volvGq";
 
-// const user: User = { id: userId, name: "Gypsy Danger" }; // type anonimous and with my API secret I can generate a token to join the call 
+// const user: User = { id: userId, name: "Tutorial" };
 // const client = new StreamVideoClient({ apiKey, user, token });
 // const call = client.call("livestream", callId);
 // call.join({ create: true });
-
-// // Viwer by default should not be able to appear or talk
-// call.camera.disable();
-// call.microphone.disable();
 
 // export default function Home() {
 //   return (
 //     <StreamVideo client={client}>
 //       <StreamCall call={call}>
-//         <LivestreamLayout 
-//           showParticipantCount
-//           showDuration
-//           showLiveBadge
-//         />
+//         <LivestreamView />
 //       </StreamCall>
 //     </StreamVideo>
 //   );
 // }
 
+// const LivestreamView = () => {
+//   const {
+//     useCameraState,
+//     useMicrophoneState,
+//     useParticipantCount,
+//     useIsCallLive,
+//     useParticipants,
+//   } = useCallStateHooks();
+
+//   const { camera: cam, isEnabled: isCamEnabled } = useCameraState();
+//   const { microphone: mic, isEnabled: isMicEnabled } = useMicrophoneState();
+  
+//   const participantCount = useParticipantCount();
+//   const isLive = useIsCallLive();
+
+//   const [firstParticipant] = useParticipants();
+  
+//   return (
+//     <div style={{ display: "flex", flexDirection: 'column', gap: '4px' }}>
+//       <div>{isLive ? `Live: ${participantCount}`: `In Backstage`}</div>
+//       {firstParticipant ? (
+//         <ParticipantView participant={firstParticipant} />
+//       ) : (
+//         <div>The host hasn't joined yet</div>
+//       )}
+//       <div style={{ display: 'flex', gap: '4px'}}>
+//         <button onClick={() => (isLive ? call.stopLive() : call.goLive())}>
+//           {isLive ? "Stop Live" : "Go Live"}
+//         </button>
+//         <button onClick={() => cam.toggle()}>
+//           {isCamEnabled ? "Disable camera" : "Enable camera"}
+//         </button>
+//         <button onClick={() => mic.toggle()}>
+//           {isMicEnabled ? "Mute Mic" : "Unmute Mic"}
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
