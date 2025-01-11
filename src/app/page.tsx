d854e728f5ac0ec5dc55c64cc8bd80c5d1db6132
Jorge Zerpa <jorgezerpacoder@gmail.com>
2025-01-11 07:19:08 -0400

    basic NextAuth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
+'use client'
+// export default function Home() {
+//   return (
+//     <div className="flex justify-center items-center h-24">
+//       <div className="font-bold text-2xl cursor-pointer">
+//         Register
+//       </div>
+//     </div>
+//   );
+// }
+
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
+  const { data: session } = useSession()
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session?.user?.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+  
   return (
-    <div className="flex justify-center items-center h-24">
-      <div className="font-bold text-2xl cursor-pointer">
-        Register
-      </div>
-    </div>
-  );
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
 }
