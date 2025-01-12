71e7efb0714d977d275a94b0cc6f127ffe8d604a
Jorge Zerpa <jorgezerpacoder@gmail.com>
2025-01-12 18:34:27 -0400

    FInish logic of streams view on client dashboard

--- src/app/(authRequired)/userDashboard/streams/page.tsx
+++ src/app/(authRequired)/userDashboard/streams/page.tsx
@@ -6,13 +6,33 @@ import axios from "axios"
 export default function Streams() {
     const router = useRouter()
 
-    const [calls, setCalls] = useState<any>([])
+    const [calls, setCalls] = useState<any[]>([])
+
+
+
+    async function getCalls() {
+        const calls = await axios.get("/api/stream/call/get_calls")
+
+        const sortedCalls = calls?.data?.calls?.sort((a:any, b:any) => {
+        const dateA = new Date(a.createdAt);
+        const dateB = new Date(b.createdAt);
+    
+        // Reverse the comparison for descending order
+        if (dateA < dateB) {
+            return 1; // b comes before a
+        } else if (dateA > dateB) {
+            return -1; // a comes before b
+        } else {
+            return 0; // a and b have the same createdAt
+        }
+        }) || [];
+        return sortedCalls
+    }
 
     useEffect(()=>{
         (async()=>{
-            const response = await axios.get("/api/stream/call/get_calls")
-            const responseCalls = response.data.calls?.reverse()
-            setCalls(responseCalls)
+            const sortedCalls = await getCalls()
+            setCalls(sortedCalls)
         })()
     },[])
 
@@ -21,8 +41,28 @@ export default function Streams() {
             <h1 className="text-center pt-7 pb-7 font-bold text-2xl">Streams</h1>
 
             <div className="flex justify-center items-center py-24">
-                <div onClick={()=>router.push(`/ViewLiveStream?livestream-id=${calls[0]?.callId}`)} className="cursor-pointer text-center text-lg font-bold text-white px-4 py-2 bg-purple-700 rounded-sm">
+                <div onClick={()=>router.push(`/viewLiveStream?livestream-id=${calls[0]?.callId}`)} className="cursor-pointer text-center text-lg font-bold text-white px-4 py-2 bg-purple-700 rounded-sm">
                     Join Stream { calls[0]?.callId }
+                </div>
+            </div>
+
+            <div>
+                <h4 className="mt-5 text-lg text-center font-bold mb-2">Previous streams</h4>
+                <div className="px-20">
+                {
+                    calls.map((call, index) => {
+                    return (
+                        <div key={"listofcallsforclientdashboardstreamsroute"+index} className="mb-4">
+                        <p>Call_id: { call.callId }</p>
+                        <p>state: { call.state }</p>
+                        {
+                            (index === 0) &&
+                            <button className="cursor-pointer" type="button" onClick={()=>router.push(`/viewLiveStream?livestream-id=${calls[0]?.callId}`)}>Join Stream</button>
+                        }
+                        </div>
+                    )
+                    })
+                }
                 </div>
             </div>
 
