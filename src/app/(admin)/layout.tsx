562f459ded39837bc39b6247e2869c424f332bd4
Jorge Zerpa <jorgezerpacoder@gmail.com>
2025-01-12 12:20:35 -0400

    create admin routes

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,22 @@
+import { Navbar } from "@/components/Navbar";
+import { Sidebar } from "@/components/Sidebar";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+        <Navbar />
+        <div className="flex justify-start items-start">
+            <div className="h-screen w-[200px]">
+                <Sidebar isAdmin />
+            </div>
+            <div className="h-screen w-full">
+                { children }
+            </div>
+        </div>
+    </div>
+  );
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,28 +4,55 @@ import Link from 'next/link'
 import { signIn, signOut } from 'next-auth/react'
 import { useSession } from 'next-auth/react' 
 
-function Sidebar() {
+function Sidebar({ isAdmin }:{ isAdmin?:boolean }) {
     const { data: session, status } = useSession()
 
   return (
-    <div
-        className='h-full flex flex-col justify-start items-start py-4 px-4 bg-white shadow-md text-gray-900'
-    >
+    <>
+        {
+            !isAdmin && (
+                <div
+                    className='h-full flex flex-col justify-start items-start py-4 px-4 bg-white shadow-md text-gray-900'
+                >
+                    <div>
+                        <Link href="/userDashboard/streams">Streams</Link>
+                    </div>
+                    <div>
+                        <Link href="/userDashboard/billing">Billing</Link>
+                    </div>
+                    <div>
+                        <Link href="/userDashboard/profile">Profile</Link>
+                    </div>
+                
+                    <div onClick={async()=>await signOut({ callbackUrl:"/" })}>
+                        Sign Out
+                    </div>
+                </div>
+            )
+        }
+        {
+            isAdmin && (
+                <div
+                    className='h-full flex flex-col justify-start items-start py-4 px-4 bg-white shadow-md text-gray-900'
+                >
+                    <div>
+                        <Link href="/dashboard/streams">Streams</Link>
+                    </div>
+                    <div>
+                        <Link href="/dashboard/users">Users</Link>
+                    </div>
+                    <div>
+                        <Link href="/dashboard/billing">Billing</Link>
+                    </div>
 
-        <div>
-            <Link href="/userDashboard/streams">Streams</Link>
-        </div>
-        <div>
-            <Link href="/userDashboard/billing">Billing</Link>
-        </div>
-        <div>
-            <Link href="/userDashboard/profile">Profile</Link>
-        </div>
-       
-        <div onClick={async()=>await signOut({ callbackUrl:"/" })}>
-            Sign Out
-        </div>
-    </div>
+                
+                    <div onClick={async()=>await signOut({ callbackUrl:"/" })}>
+                        Sign Out
+                    </div>
+                </div>
+            )
+        }
+    </>
   )
 }
 
