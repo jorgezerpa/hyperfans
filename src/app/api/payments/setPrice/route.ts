1ec5dbdd1d39a8ccb838c030994c370971bcd72a
Jorge Zerpa <jorgezerpacoder@gmail.com>
2025-01-16 08:52:59 -0400

    partial

--- src/app/api/payments/setPrice/route.ts
+++ src/app/api/payments/setPrice/route.ts
@@ -1,7 +1,13 @@
-import { NextResponse } from "next/server"
 import {prisma} from "../../../../../lib/prisma"
 
+import { authOptions } from "../../auth/[...nextauth]/route"
+import { getServerSession } from "next-auth"
+import { NextResponse } from "next/server"
+
 export async function PUT (req: Request) {
+    const session = await getServerSession(authOptions)
+    if(session?.user.role !== "admin") return NextResponse.error()
+
     const data = await req.json()
     const config = await prisma.config.findFirst()
     if(!config) return null
--- src/app/api/stream/call/create_call/route.ts
+++ src/app/api/stream/call/create_call/route.ts
@@ -1,7 +1,12 @@
 import { NextResponse } from "next/server"
 import {prisma} from "../../../../../../lib/prisma"
+import { authOptions } from "../../../auth/[...nextauth]/route"
+import { getServerSession } from "next-auth"
 
 export async function POST (req: Request) {
+    const session = await getServerSession(authOptions)
+    if(session?.user.role !== "admin") return NextResponse.error()
+        
     const { callId, state } = await req.json()
 
     await prisma.call.create({
