1bd2f5a1fe387c4b9b9cab4f5313fd473adfdff4
Jorge Zerpa <jorgezerpacoder@gmail.com>
2025-01-16 22:32:56 -0400

    Set web3 constants to env variables

--- src/constants/web3.ts
+++ src/constants/web3.ts
@@ -1,3 +1,3 @@
-export const WALLET_TO_PAY = "0x278C45B373c9EAAAAb20BA290F321429b542Cd6F" // arbitrum
-export const RPC_ENDPOINT = "https://arbitrum-sepolia.infura.io/v3/b26a78bcb38b4957a68b3cdc645c2547"
-export const USDC_CONTRACT = "0xaf88d065e77c8cC2239327C5EDb3A432268e5831" // arbitrum
+export const WALLET_TO_PAY = process.env.NEXT_PUBLIC_WALLET_TO_PAY // arbitrum
+export const RPC_ENDPOINT = process.env.NEXT_PUBLIC_RPC_ENDPOINT
+export const USDC_CONTRACT = process.env.NEXT_PUBLIC_USDC_CONTRACT // arbitrum
