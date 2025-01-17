956f103b6918786ba8948a65f0e122bd1ddb5747
Jorge Zerpa <jorgezerpacoder@gmail.com>
2025-01-17 07:47:54 -0400

    change stream dashboard url

--- src/app/(admin)/dashboard/streams/page.tsx
+++ src/app/(admin)/dashboard/streams/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useEffect, useState } from "react"
 import axios from "axios"
+import { STREAM_DASHBOARD_LINK } from "@/constants/web3"
 
 export default function Streams() {
   const [loading, setLoading] = useState(false)
@@ -93,7 +94,7 @@ export default function Streams() {
         }} 
         className="flex flex-col gap-4 items-center sm:items-start">
         <h4 className="text-lg font-bold mr-2">New stream:</h4>
-        <p>Create a new stream on the <a href="https://dashboard.getstream.io/app/1358593/video/overview" target="_blank" className="text-blue-600">Stream.io</a> dashboard an paste the Livestream ID down here to share with all users</p>
+        <p>Create a new stream on the <a href={STREAM_DASHBOARD_LINK} target="_blank" className="text-blue-600">Stream.io</a> dashboard an paste the Livestream ID down here to share with all users</p>
         <div className="flex  items-center">
           <input name={"stream-id"} value={newCallId} onChange={(e)=>setNewCallId(e.target.value)} type="text" placeholder="id" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-[200px] p-2.5" />
           <button disabled={loading||!newCallId} style={{ opacity:(newCallId)?1:.3 }} className="px-4 py-2 text-white bg-purple-700 rounded-lg ml-2 hover:bg-purple-800 cursor-pointer">Share</button>
